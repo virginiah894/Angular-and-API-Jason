ee229f2b3cb4c9488a5428b70c9c4a388bffcfe0
virginiah894 <virgyperry@gmail.com>
2020-03-03 17:14:06 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularApi';
+export class AppComponent implements OnInit {
+  users$: User[];
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    return this.dataService.getUsers().subscribe(data => this.users$ = data);
+  }
 }
